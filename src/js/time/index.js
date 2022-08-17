b7fe3fb639f7531c58b32f51a2c6d6c0e6dcd8e0
RomanovaCute <romanova17romanova@yandex.ru>
2022-08-17 20:49:26 +0500

    feat: add weekday

--- src/js/time/index.js
+++ src/js/time/index.js
@@ -44,7 +44,7 @@ function showTime() {
 
 function showDate() {
     const date = new Date();
-    const options = {month: 'long', day: 'numeric'};
+    const options = {weekday: 'long', month: 'long', day: 'numeric'};
     const currentDate = date.toLocaleDateString('en-US', options);
     dateBox.textContent = currentDate;
 }
