151cb281c2c4860a1590f4c05b41f5e9b7f74a70
RomanovaCute <romanova17romanova@yandex.ru>
2022-08-17 17:06:34 +0500

    fix bugs with packaje.json & paths

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import 'src/js/time/index';
-import 'src/js/weather/index';
-import 'src/js/quotes/index'
-import 'src/js/player/index'
-import 'src/js/settings/index'
+import './src/js/time';
+import './src/js/weather/index';
+import './src/js/quotes/index'
+import './src/js/player/index'
+import './src/js/settings/index'
