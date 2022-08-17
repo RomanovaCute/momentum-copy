c6d5ff5ce50fe208a9cb09dafc113154b2c8b926
RomanovaCute <romanova17romanova@yandex.ru>
2022-08-17 20:25:22 +0500

    checking commit

--- src/js/playList/index.js
+++ src/js/playList/index.js
@@ -1,22 +1,22 @@
 const playList = [
     {
         title: 'Aqua Caelestis',
-        src: 'assets/sounds/Aqua_Caelestis.mp3',
+        src: './assets/sounds/Aqua_Caelestis.mp3',
         duration: '00:58'
     },
     {
         title: 'Ennio Morricone',
-        src: 'assets/sounds/Ennio_Morricone.mp3',
+        src: './assets/sounds/Ennio_Morricone.mp3',
         duration: '00:58'
     },
     {
         title: 'River Flows In Your',
-        src: 'assets/sounds/River_Flows_In_Your.mp3',
+        src: './assets/sounds/River_Flows_In_Your.mp3',
         duration: '03:50'
     },
     {
         title: 'Summer Wind',
-        src: 'assets/sounds/Summer_Wind.mp3',
+        src: './assets/sounds/Summer_Wind.mp3',
         duration: '00:58'
     },
 ]
