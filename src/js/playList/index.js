2c74b7ecae96e6b8986c5e47765be2ae2865715b
RomanovaCute <romanova17romanova@yandex.ru>
2022-08-28 19:59:37 +0500

    fix: fix playList

--- src/js/playList/index.js
+++ src/js/playList/index.js
@@ -7,17 +7,17 @@ const playList = [
     {
         title: 'Ennio Morricone',
         src: './assets/sounds/Ennio_Morricone.mp3',
-        duration: '00:58'
+        duration: '01:38'
     },
     {
         title: 'River Flows In Your',
-        src: './assets/sounds/River_Flows_In_Your.mp3',
-        duration: '03:50'
+        src: './assets/sounds/River_Flows_In_You.mp3',
+        duration: '01:36'
     },
     {
         title: 'Summer Wind',
         src: './assets/sounds/Summer_Wind.mp3',
-        duration: '00:58'
+        duration: '01:50'
     },
 ]
 
